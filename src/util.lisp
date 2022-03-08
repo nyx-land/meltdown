831ba96e15a2fcf9169cd12be3a302f67b04d2ad
Nyx <n1x@riseup.net>
2022-03-07 23:27:57 -0800

    refactor helper functions

--- src/util.lisp
+++ src/util.lisp
@@ -1,27 +1,8 @@
 (in-package :meltdown)
 
-(defun getchar (c)
-  (flet ((lc (cx) (equalp c cx))
-         (ret (obj) `(make-instance (quote ,obj))))
-    (cond ((lc #\#) (ret 'heading))
-          (t (ret 'section)))))
-
-(defun make-obj (c &key (eval? t))
-  (let ((obj (getchar c)))
-    (if eval?
-        (eval obj)
-        obj)))
-
 (defun cleanup-title (s)
   (remove #\Space
           (remove #\# s)))
-
-(defun add-subheading (parent)
-  (push (eval (append
-               (make-obj #\# :eval? nil)
-               `(:depth ,(+ 1 (depth parent))
-                 :parent ,parent)))
-        (children parent)))
 
 (defun collect-paragraphs (in-stream)
   (loop for x = (read-char in-stream nil)
@@ -33,3 +14,33 @@
   (remove-if (lambda (x) (equalp "" x))
              (uiop:split-string text
                                 :separator (format nil "~%"))))
+
+(defun ret-node (depth parent)
+  (let ((ret `(:depth ,depth)))
+    (if parent (append ret `(:parent ,parent))
+        ret)))
+
+(defun make-heading-param (in-stream &key (parent nil))
+  (loop for c = (read-char in-stream nil)
+        for d = 1 then (incf d)
+        when (not (eql c #\#))
+          return (ret-node d parent)))
+
+(defun make-heading (in-stream &key (parent nil))
+  (eval (append
+         '(make-instance (quote heading))
+         (make-heading-param in-stream :parent parent))))
+
+(defun heading-handler (in-node depth pos)
+  (cond ((< depth (depth pos))
+         (setf (parent in-node) pos))
+        ((= depth (depth pos))
+         (if (parent pos)
+             (setf (parent in-node) (parent pos))))))
+
+(defun make-obj (c &key in-stream (parent nil))
+  (flet ((lc (cx) (equalp c cx))
+         (ret (obj) `(make-instance (quote ,obj))))
+    (cond ((lc #\#) (make-heading in-stream
+                                  :parent parent))
+          (t (ret 'section)))))
