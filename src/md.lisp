19dea837fdc6ab742b79484df8fffa40ab5b06a8
Nyx <n1x@riseup.net>
2022-03-14 17:19:01 -0700

    add docstrings

--- src/md.lisp
+++ src/md.lisp
@@ -10,13 +10,17 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 
 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 
-At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."))
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.")
+  "A test markdown document. TODO: make proper tests for this")
 
 ;;(defclass token ()
 ;;  ((c
 ;;    :initarg :char
 ;;    :accessor char)))
 
+;; A document's lifecycle begins as a struct with some basic
+;; information to help the parser methods track the position
+;; within a document.
 (defstruct doc-raw
   nodes pos raw)
 
@@ -24,7 +28,8 @@ At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praese
   ((nodes
     :initarg :nodes
     :initform nil
-    :accessor nodes)))
+    :accessor nodes))
+  (:documentation "The whole markdown document."))
 
 (defclass node ()
   ((parent
@@ -34,7 +39,9 @@ At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praese
    (children
     :initarg :children
     :initform nil
-    :accessor children)))
+    :accessor children))
+  (:documentation "A node is something that marks a change in the
+hierachy of a document."))
 
 (defclass heading (node)
   ((title
@@ -47,7 +54,9 @@ At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praese
    (sections
     :initarg :sections
     :initform nil
-    :accessor sections)))
+    :accessor sections))
+  (:documentation "A heading is a type of node that has a title
+associated with it and a depth."))
 
 (defclass section (node)
   ((paragraphs
--- src/util.lisp
+++ src/util.lisp
@@ -1,10 +1,16 @@
+;; This has all the business logic for figuring out what to do with
+;; the contents of a markdown file/string after it's been passed
+;; through the toplevel parser method.
+
 (in-package :meltdown)
 
 (defun cleanup-title (s)
+  "Remove extraneous characters from the title of a heading."
   (remove #\Space
           (remove #\# s)))
 
 (defun collect-paragraphs (in-stream)
+  "Collect the raw string for the paragraph(s) that belong to a heading."
   (loop for x = (read-char in-stream nil)
         until (or (eql x #\#)
                   (null x))
@@ -12,27 +18,34 @@
         finally (return (concatenate 'string text))))
 
 (defun split-paragraphs (text)
+  "Split a string into paragraphs."
   (remove-if (lambda (x) (equalp "" x))
              (uiop:split-string text
                                 :separator (format nil "~%"))))
 
 (defun ret-node (depth parent)
+  "If the heading has a parent, return that along with its depth."
   (let ((ret `(:depth ,depth)))
     (if parent (append ret `(:parent ,parent))
         ret)))
 
 (defun make-heading-param (in-stream &key (parent nil))
+  "Determine the heading's depth and whether or not it has a parent."
   (loop for c = (read-char in-stream nil)
         for d = 1 then (incf d)
         when (not (eql c #\#))
           return (ret-node d parent)))
 
 (defun make-heading (in-stream &key (parent nil))
+  "A special object constructor to handle making a heading. Appends
+the unevaluated parameters to the constructor before making the object."
   (eval (append
          '(make-instance (quote heading))
          (make-heading-param in-stream :parent parent))))
 
 (defun heading-handler (in-node depth pos)
+  "A handler when a heading is received to figure out where the
+heading belongs within the document's hierarchy."
   (cond ((< depth (depth pos))
          (setf (parent in-node) pos))
         ((= depth (depth pos))
@@ -40,6 +53,10 @@
              (setf (parent in-node) (parent pos))))))
 
 (defun make-obj (c &key in-stream (parent nil))
+  "Make an object that corresponds to a node in the markdown
+document based off the character that is received by the parser
+method. Return the doc if it reaches the end of the stream and
+by default assume a character belongs to a paragraph."
   (flet ((lc (cx) (equalp c cx))
          (ret (obj) `(make-instance (quote ,obj))))
     (cond ((lc #\#) (make-heading in-stream
